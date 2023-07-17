0692e085d0692c18f47cc699f4b42fff644a19ed
americanoc11 <hhd0511469699@gmail.com>
2023-07-17 13:31:47 +0700

    update

---
+++ ss1/bai_tap/ex_1.js
@@ -0,0 +1,72 @@
+let courses = [
+    {
+        id: 1,
+        title: "ReactJS Tutorial",
+        rating: 4.2,
+    },
+    {
+        id: 2,
+        title: "Angular Tutorial",
+        rating: 2.5,
+    },
+    {
+        id: 3,
+        title: "VueJS Tutorial",
+        rating: 3.8,
+    },
+    {
+        id: 4,
+        title: "Java Tutorial",
+        rating: 4,
+    },
+    {
+        id: 5,
+        title: "JavaScript Tutorial",
+        rating: 3.5,
+    },
+];
+
+let addedCourses = [
+    {
+        id: 6,
+        title: "PHP Tutorial",
+        rating: 3,
+    },
+    {
+        id: 7,
+        title: "C# Tutorial",
+        rating: 2,
+    },
+    {
+        id: 8,
+        title: "Docker Tutorial",
+        rating: 3.8,
+    }
+];
+
+// ex1
+let goodRate = courses.filter(function (courses) {
+    return courses.rating >= 4;
+});
+console.log(goodRate);
+
+// ex2
+let badRate = courses.filter(function (courses) {
+    return courses.rating < 4;
+});
+badRate.forEach(function (courses) {
+    console.log(courses.id + " - " + courses.title + " - " + courses.rating);
+});
+
+// ex3
+
+// courses.map(function(courses){
+//     return courses;
+//     })
+// addedCourses.forEach(function(addedCourses){
+//     courses.push(addedCourses);
+// })
+// console.log(courses);
+
+let newArray=[...courses,...addedCourses];
+console.log(newArray);
