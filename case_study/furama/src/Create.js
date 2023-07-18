9ae63a204ec2823615df237a7df5a65c7917111d
americanoc11 <hhd0511469699@gmail.com>
2023-07-18 21:11:33 +0700

    update

---
+++ case_study/furama/src/Create.js
@@ -0,0 +1,27 @@
+export default function Create() {
+    return (
+        <>
+<div className="container">
+    <div style="width: 600px;">
+        <form>
+            <h3 style={{textAlign: "center"}}>Add New Service</h3>
+            <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label">Name Service</label>
+                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label for="exampleInputPassword2" className="form-label">Area</label>
+                <input type="text" className="form-control" id="exampleInputPassword2"/>
+            </div>
+            <div className="mb-3">
+                <label for="exampleInputPassword3" className="form-label">Price</label>
+                <input type="text" className="form-control" id="exampleInputPassword3"/>
+            </div>
+            <button type="submit" className="btn btn-outline-primary">Create</button>
+        </form>
+    </div>
+</div>
+        </>
+    )
+}
